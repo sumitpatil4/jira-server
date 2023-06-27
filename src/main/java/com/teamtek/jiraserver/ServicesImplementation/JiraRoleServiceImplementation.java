af1a1f73d7515ca40a942e7dc420b00d7df85adb
Pratyush <pratyush.singh@accolitedigital.com>
2023-06-27 11:28:49 +0530

    Add JiraRole and Get all JiraRole API implemented and tested too

--- src/main/java/com/teamtek/jiraserver/ServicesImplementation/JiraRoleServiceImplementation.java
+++ src/main/java/com/teamtek/jiraserver/ServicesImplementation/JiraRoleServiceImplementation.java
@@ -1,8 +1,32 @@
 package com.teamtek.jiraserver.ServicesImplementation;
 
+import com.teamtek.jiraserver.Model.JiraRole;
+import com.teamtek.jiraserver.Repository.JiraRoleRepository;
 import com.teamtek.jiraserver.Services.JiraRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class JiraRoleServiceImplementation implements JiraRoleService {
+
+    @Autowired
+    private JiraRoleRepository jiraRoleRepository;
+
+    @Override
+    public ResponseEntity<String> addNewRole(String role) {
+        JiraRole jiraRole = new JiraRole();
+        jiraRole.setRole(role);
+        this.jiraRoleRepository.save(jiraRole);
+        return new ResponseEntity<>("Role Created Successfully", HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<JiraRole>> getAllRole() {
+        List<JiraRole> jiraRoles = this.jiraRoleRepository.findAll();
+        return new ResponseEntity<>(jiraRoles,HttpStatus.OK);
+    }
 }
